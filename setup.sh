6387a083a595870e2a3b100f94e356ba6aec2790
jack <obri.jack.02@gmail.com>
2024-07-12 09:14:53 +1000

    Fixed sh script type

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
-cd ~
-mv dotfiles/ .dotfiles/
+mv -T ~/dotfiles ~/.dotfiles
 ln -s .dotfiles/.tmux.conf
 ln -s .dotfiles/.vimrc
+
+# Bashrc setup
+echo 'source .dotfiles/unlimited_bash_history.sh' >> ~/.bashrc
+echo 'source .dotfiles/vim_bash_keybindings.sh' >> ~/.bashrc
