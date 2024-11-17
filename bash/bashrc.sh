86124058de4770ac37037eaddef46e592edb4a54
jack <obri.jack.02@gmail.com>
2024-11-17 21:40:41 +1000

    Moved bash shell config to its own directory

---
+++ bash/bashrc.sh
@@ -0,0 +1,14 @@
+# This is intended to be the single interface between the dotfiles and the .bashrc file.
+# So during the setup phase, all we need to do is add "source ~/.config/dotfiles/bash/bashrc.sh" to the .bashrc file.
+# Then I can continue making any future changes I want, and there is no further setup needed!
+
+DOTFILES_DIR=$( dirname -- "${BASH_SOURCE[0]}" )
+
+source $DOTFILES_DIR/unlimited_bash_history.sh
+source $DOTFILES_DIR/vim_bash_keybindings.sh
+source $DOTFILES_DIR/history_search.sh
+
+# alias rm to build safer habits
+alias rm="echo Please use trash or /bin/rm"
+
+alias histroy=history
